8c940c48eff0a763cc9f4aa736edb965078ce92c
Vrundal-Shah <vrundalshah05@gmail.com>
2020-11-09 06:17:25 +0300

    initial deploy

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React from 'react';
-import {} from 'recat-router-dom';
+// import {} from 'recat-router-dom';
 
 function App() {
   return (
     // **************TODO******************* ADD ROUTER
     <div className="App">
+    <h1>Vrundal Shah</h1>
     <h1>Coming Soon</h1>
     </div>
   );
